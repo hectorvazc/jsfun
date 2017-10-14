a5784f9b9c32b5bc3322ac61453207ebd244b00c
hector vazquez <hectorvazc@gmail.com>
2017-10-14 15:22:50 -0500

    init

---
+++ jsfun.js
@@ -0,0 +1,15 @@
+var jsfn = function jsfn(element){
+    this.element = element;
+};
+
+jsfn.prototype.find = function(){
+
+}
+
+
+
+window.onload = function(){
+    (function(){
+        var js = new jsfn();
+    })(this);
+};
