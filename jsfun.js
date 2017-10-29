fb2d0e6206327216cf4015c80f5f977882b2e33f
hector vazquez <hectorvazc@gmail.com>
2017-10-29 17:26:25 -0600

    v1.0.2

--- jsfun.js
+++ jsfun.js
@@ -1,264 +1,334 @@
 var debFlag = true;
-var log = function log(text, active){
-	if((active === undefined || active) && debFlag){
-		console.log(text);
-	}
+var log = function log(toLog, active) {
+	if ((active === undefined || active) && debFlag) console.log(toLog);
 };
 
-var js = function js(element){
+var js = function js(element) {
+	if (!(this instanceof js)) return new js(element);
+	if (!isNode(element)) element = find_selector(element);
 	this.element = element;
+	this.context = undefined;
 };
 
-js.prototype.get = function(){
-	var element = this.element;
-	js.clean();
-	return element;
-}
+js.prototype.get = function () { return this.element; }
 
-js.prototype.set = function(element){
-	this.element = element;
+js.prototype.find = function (selector) {
+	this.element = find_selector(selector, this.context);
+	this.context = undefined;
 	return this;
-}
+};
 
-js.prototype.find = function(selector) {
-
-	var query_result;
-	if (this.context === undefined) {
-		query_result = find_selector(selector);
-	} else {
-		query_result = find_selector(selector, this.context);
-	}
-
-	if (query_result.length == 0) {
-		this.element = undefined;
-	} else if (query_result.length == 1) {
-		this.element = query_result[0];
-	} else {
-		this.element = query_result;
+js.prototype.here = function (selector) {
+	if (isNode(selector)) this.context = selector;
+	else {
+		var query_result = find_selector(selector);
+		if (query_result.length > 1) this.context = query_result[0];
 	}
 	return this;
 };
 
-js.prototype.here = function(selector) {
-	var query_result = find_selector(selector);
-	if (query_result.length == 0) {
-		this.context = undefined;
-	} else if (query_result.length == 1) {
-		this.context = query_result[0];
-	} else {
-		this.context = query_result;
-	}
-	return this;
-};
-
-js.prototype.html = function(text){
+js.prototype.html = function (text) {
+	if (text === undefined) return this.element.innerHTML;
 	this.element.innerHTML = text;
 	return this;
 };
 
-js.prototype.empty = function(){
+js.prototype.empty = function () {
 	this.element.innerHTML = '';
 	return this;
 };
 
-js.prototype.append = function(text){
-	this.element.innerHTML = this.element.innerHTML + " " + text;
+js.prototype.append = function (text) {
+	this.element.innerHTML = [this.element.innerHTML, text].join(" ");
 	return this;
 };
 
-js.prototype.event = function(events, fn, optional_identifier){
-	if(this.element === undefined){ 
-		return false;
-	}
-	event_bridge(this.element, events, fn, optional_identifier);
-	js.clean();
+js.prototype.exist = function (selector) {
+	return this.element !== undefined;
 };
 
-js.prototype.exist = function(selector){
-	var element_exist = js.find(selector).element !== undefined;
-	js.clean();
-	return element_exist;
-};
-
-js.prototype.val = function(value){
-	if(value === undefined){
-		var value = this.element.value;
-		js.clean();
-		return value;
+js.prototype.val = function (value) {
+	if (this.element === undefined) return undefined;
+	if (value === undefined) {
+		return this.element.value;
 	}
-
 	this.element.value = value;
 	return this;
 };
 
-js.prototype.fn = function(fn, identifier){
-	(function(){ 
-		if(identifier !== undefined){
-			log(identifier);
-		}
-		fn();
-	})(this);
-}
-
-js.prototype.data = function(data){
-	return $data(this.element, data);
-}
-
-js.prototype.clean = function(){
+js.prototype.clean = function () {
 	this.element = undefined;
+	this.context = undefined;
 };
 
-js.prototype.addAttr = function(attributeList){
-	if(this.element === undefined) return false; // if element its undefined lets return
-	if(!isObject(attributeList)) return false; // attributes to add must be an object.
-	
-	Array.prototype.forEach.call(ensureArray(this.element), function(element){
-		for(var attr in attributeList){
-			element.setAttribute(attr, attributeList[attr]);
-		}
-	});
+js.prototype.first = function () {
+	this.element = $array(this.element)[0];
 	return this;
 };
 
-js.prototype.removeAttr = function(attributeList){
-	if(this.element === undefined) return false; // if element its undefined lets return
-	Array.prototype.forEach.call(ensureArray(this.element), function(element){
-		Array.prototype.forEach.call(ensureArray(attributeList), function(attribute){
-			element.removeAttribute(attribute);
-		});
-	});
-	return this;
-};
-
-js.prototype.addStyle = function(styleList){
-	if(this.element === undefined) return false;
-	if(!isObject(styleList)) return false; // styles to add must be an object.
-	
-	Array.prototype.forEach.call(ensureArray(this.element), function(element){
-		for(var style in styleList){
-			element.style.setProperty(style, styleList[style]);
-		}
-	});
-	return this;
-};
-
-
-js.prototype.removeStyle = function(styleList){
-	if(this.element === undefined) return false;
-	Array.prototype.forEach.call(ensureArray(this.element), function(element){
-		Array.prototype.forEach.call(ensureArray(styleList), function(style){
-			 element.style.removeProperty(style);
-		});
-	});
-	return this;
-};
-
-js.prototype.hide = function(){
-	if(this.element === undefined) return false;
-	Array.prototype.forEach.call(ensureArray(this.element), function(element){
-		element.style.setProperty("display", "none");
-	});
-};
-
-js.prototype.show = function(){
-	if(this.element === undefined) return false;
-	Array.prototype.forEach.call(ensureArray(this.element), function(element){
-		element.style.removeProperty("display");
-	});
-};
-
-var js = new js(null);	// init the fluent api
-
-
-var ensureArray = function ensureArray(inside_element){
-	var temp_arr;
-	if(!isNodeList(inside_element) && !isArray(inside_element)){
-		temp_arr = [];
-		temp_arr.push(inside_element);
-	}else{
-		temp_arr = inside_element;
-	}
-	return temp_arr;
-}
-
-
-//js.prototype.removeAttr = function(attributes){
-//	
-//};
-
-/* =======================================================
- * 					 _NEW ONES_
- * ====================================================== */
-
-var find_selector = function find_selector(selector, context) {
-	if (context === undefined) {
-		return document.querySelectorAll(selector);
-	} else {
-		return context.querySelectorAll(selector);
-	}
-};
-
-var $data = function $data(element, data){
-	var dataset = element.dataset[data] || element.dataset;
-	if(dataset !== undefined){
-		if(dataset.length === undefined){
-			dataset = undefined;
-		}
-	}
-	return dataset;
-};
-
-var event_bridge = function event_bridge(element, type, fn, optional_identifier){
-	if(isString(type)){
-		if(isNodeList(element)){
-			Array.prototype.forEach.call(element, function(el){
-				bind_event(el, type, fn, optional_identifier);
-			});
-		}else{
-			bind_event(element, type, fn, optional_identifier);
-		}
-	}else{
-		Array.prototype.forEach.call(type, function(typefn){
-			if(isNodeList(element)){
-				Array.prototype.forEach.call(element, function(el){
-					bind_event(el, typefn, fn, optional_identifier);
-				});
-			}else{
-				bind_event(element, typefn, fn, optional_identifier);
+js.prototype.setAttribute = function (attributeList) {
+	if (typeof (attributeList) !== 'object') return false; // attributes to add must be an object.
+	if (this.element !== undefined) {
+		foreach(this.element, function (element) {
+			for (var attr in attributeList) {
+				element.setAttribute(attr, attributeList[attr]);
 			}
 		});
+	} else {
+		log("this.element is undefined. setAttribute call.");
+	}
+	return this;
+};
+
+js.prototype.removeAttribute = function (attributeList) {
+	if (this.element !== undefined) {
+		foreach(this.element, function (element) {
+			foreach(attributeList, function (attribute) {
+				element.removeAttribute(attribute);
+			});
+		});
+	} else {
+		log("this.element is undefined. removeAttribute call.");
+	}
+	return this;
+};
+
+js.prototype.addStyle = function (styleList) {
+	if (typeof (styleList) !== 'object') return false; // styles to add must be an object.
+	if (this.element !== undefined) {
+		foreach(this.element, function (element) {
+			for (var style in styleList) {
+				element.style.setProperty(style, styleList[style]);
+			}
+		});
+	} else {
+		log("this.element is undefined. addStyle call.");
+	}
+	return this;
+};
+
+js.prototype.removeStyle = function (styleList) {
+	if (this.element !== undefined) {
+		foreach(this.element, function (element) {
+			foreach(styleList, function (style) {
+				element.style.removeProperty(style);
+			});
+		});
+	} else {
+		log("this.element is undefined. removeStyle call.");
+	}
+	return this;
+};
+
+js.prototype.addClass = function (classList) {
+	if (this.element !== undefined) {
+		foreach(this.element, function (element) {
+			foreach(classList, function (_class) {
+				element.classList.add(_class);
+			});
+		});
+	} else {
+		log("this.element is undefined. addClass call.");
+	}
+	return this;
+};
+
+js.prototype.removeClass = function (classList) {
+	if (this.element !== undefined) {
+		foreach(this.element, function (element) {
+			foreach(classList, function (_class) {
+				if (element.classList.contains(_class)) {
+					element.classList.remove(_class);
+				}
+			});
+		});
+	} else {
+		log("this.element is undefined. removeClass call.");
+	}
+	return this;
+};
+
+js.prototype.hide = function () {
+	if (this.element !== undefined) {
+		foreach(this.element, function (element) { element.style.setProperty("display", "none"); });
+	} else {
+		log("this.element is undefined. hide call.");
+	}
+	return this;
+};
+
+js.prototype.show = function () {
+	js.each(function (element) { element.style.removeProperty("display"); });
+	return this;
+};
+
+js.prototype.resize = function () {
+	if (this.element === undefined) return;
+	this.element.style.height = "1px";
+	this.element.style.height = (this.element.scrollHeight) + "px";
+	return this;
+};
+
+js.prototype.text = function () {
+	if (this.element === undefined) return '';
+	return this.element.innerText || this.element.textContent;
+};
+
+js.prototype.insertFirst = function (element_toappend) {
+	if (this.element === undefined) return;
+	this.element.insertAdjacentElement("afterbegin", element_toappend);
+	return this;
+};
+
+js.prototype.insertLast = function (element_toappend) {
+	if (this.element === undefined) return;
+	this.element.insertAdjacentElement("beforeend", element_toappend);
+	return this;
+};
+
+js.prototype.insertBefore = function (element_toappend) {
+	if (this.element === undefined) return;
+	this.element.insertAdjacentElement("beforebegin", element_toappend);
+	return this;
+};
+
+js.prototype.insertAfter = function (element_toappend) {
+	if (this.element === undefined) return;
+	this.element.insertAdjacentElement("afterend", element_toappend);
+	return this;
+};
+
+js.prototype.detach = function () {
+	if (this.element === undefined) return;
+	var elements = [];
+	foreach(this.element, function (element) {
+		elements.push($detach(element));
+	});
+	return elements;
+};
+
+js.prototype.visible = function () {
+	if (this.element === undefined) return;
+	return this.element.style.display !== 'none';
+};
+
+js.prototype.hidden = function () {
+	if (this.element === undefined) return;
+	return !js.visible();
+};
+
+js.prototype.event = function (events, fn, optional_identifier) {
+	if (this.element !== undefined) {
+		foreach(this.element, function (element) {
+			foreach(events, function (event) {
+				bind_event(element, event, fn, optional_identifier);
+			});
+		});
+		js.clean();
+	} else {
+		log("this.element is undefined. event call.");
+		return this;
 	}
 };
 
-var bind_event = function bind_event(element, event_type, fn, identifier){
+//===================================== GENERAL FUNCTIONS
+function find_selector(selector, context) {
+	var r = (context === undefined) ? document.querySelectorAll(selector) : context.querySelectorAll(selector);
+	console.assert(r.length, ["find_selector:", selector, context, "is undefined"].join(' '));
+
+	if (r.length === 0) {
+		return undefined;
+	} else if (r.length === 1) {
+		return r[0];
+	} else {
+		return r;
+	}
+};
+
+function isNode(element) {
+	return element instanceof Node;
+}
+
+function fn(fn) {
+	if (fn === undefined) return;
+	(function () { fn(); })(this);
+}
+
+function foreach(source, fn) {
+	if (source === undefined) return false;
+	$array(source).forEach(function (currentValue, index, array) {
+		fn(currentValue, index, array);
+	});
+}
+
+function $array(arr) {
+	if (arr === undefined) return [];
+	var a = [];
+	var s = /^\[object (string)\]$/gi.test({}.toString.call((arr))) || /^\[object (htmlselectelement)\]$/gi.test({}.toString.call((arr)));
+
+	if (arr.length === undefined || s) {
+		a.push(arr);
+	} else {
+		a = Array.from(arr);
+	}
+	return a;
+}
+
+function $detach(element) {
+	var parent = element.parentNode;
+	if (!parent) return;
+	parent.removeChild(element);
+	return element;
+}
+
+function bind_event(element, event_type, fn, identifier) {
 	var identifier = identifier || "events",
 		str_data = "",
 		at = [];
-	
+
 	var element_data = $data(element, identifier);
-	
-	if(element_data !== undefined){
+
+	if (element_data !== undefined)
 		str_data = element.dataset[identifier];
-	}
-	
-	if(!empty(str_data)){
+
+	if (!empty(str_data))
 		var at = getJSON(str_data);
-	}else{
+	else
 		at = [];
-	}
-	
-	if(at.length > 0){
-		if(at.indexOf(event_type) !== -1){
+
+	if (at.length > 0)
+		if (at.indexOf(event_type) !== -1)
 			return;
-		}
-	}
+
 	at.push(event_type);
-	
+
 	element.dataset[identifier] = setJSON(at);
 	element.addEventListener(event_type, fn);
 };
 
-/* =========================================
- *  	      
- * =========================================
- */
+function newElement(typeOfElement) {
+	return document.createElement(typeOfElement);
+}
+
+function key_code(event) {
+	return parseInt(event.keyCode || event.which);
+}
+
+function toJSON(obj) {
+	return JSON.stringify(obj);
+};
+
+function parseJSON(obj) {
+	return JSON.parse(obj);
+};
+
+function parent(element, deep) {
+	if (deep === undefined) {
+		return element.parentNode;
+	} else {
+		for (var t = 0; t < Number(deep); t++) {
+			element = element.parentNode;
+		}
+		return element;
+	}
+};
