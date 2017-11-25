49c346b6ff7699bee90f818d9fd244cd19bf8001
hector vazquez <hectorvazc@gmail.com>
2017-11-25 12:30:05 -0600

    LocalStorage Methods

--- jsfun.js
+++ jsfun.js
@@ -384,3 +384,16 @@ function ajax(method, url) {
         xhr.send();
     });
 }
+
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+
+Storage.prototype.getObject = function(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
+
+Storage.prototype.removeObject = function(key){
+	this.removeItem(key);
+}
